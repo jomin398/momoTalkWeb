cf79788e865f760b670a2c1ea67b9c7edc25746c
jomin398 <jomin398@gmail.com>
2021-11-16 23:28:55 +0900

    update

---
+++ src/index.js
@@ -0,0 +1,49 @@
+let MomoTalk = null;
+function momoTalk(){
+    function momoTalk(){
+        this.mainElm = null;
+        this.people = {
+            status:null,
+            chrs:null
+}
+    }
+    momoTalk.prototype.init = function(){
+        let xhr = new XMLHttpRequest();
+        document.querySelector('#init_display').remove();
+        document.body.className = 'main';
+        
+        const continer = document.querySelector('.momoTalk.main');
+        this.mainElm = continer;
+        continer.style.display= 'grid';
+        const header = document.querySelector('.momoTalk.main .header');
+        title ='MomoTalk';
+        header.insertAdjacentHTML('afterbegin',title);
+        xhr.open('get','./json/students.json');
+        xhr.onload = ()=>{
+            _data = JSON.parse(xhr.response);
+            this.people.status=_data.status;
+            console.log(this.people.status)
+            this.renderUserLists();
+        }
+        xhr.send();
+    }
+    momoTalk.prototype.renderUserLists = function(){
+        console.log(0)
+    }
+    return momoTalk;
+}
+
+function pageInit(){
+
+}
+function pageOnload(){
+    MomoTalk = new(momoTalk());
+    MomoTalk.init();
+    // pageInit();
+}
+
+window.onload = ()=>{
+    setTimeout(() => {
+        pageOnload();
+    }, 1000);
+}
