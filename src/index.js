7956a1e25f3abb542cc1c5369325d34b40d8d8d3
jomin398 <jomin398@gmail.com>
2021-11-17 22:32:41 +0900

    update

--- src/index.js
+++ src/index.js
@@ -1,48 +1,61 @@
 let MomoTalk = null;
-function momoTalk(){
-    function momoTalk(){
+function momoTalk() {
+    function momoTalk() {
         this.mainElm = null;
         this.isMob = false;
         this.people = {
-            status:null,
-            chrs:null
-}
+            rawDB: null,
+            unread:null
+        }
     }
-    momoTalk.prototype.init = function(){
+    momoTalk.prototype.init = function () {
         let xhr = new XMLHttpRequest();
         document.querySelector('#init_display').remove();
         document.body.className = 'main';
         const continer = document.querySelector('.momoTalk.main');
         this.mainElm = continer;
-        continer.style.display= 'grid';
+        continer.style.display = 'grid';
         const header = document.querySelector('.momoTalk.main .header');
-        title ='MomoTalk';
-        header.insertAdjacentHTML('afterbegin',title);
-        xhr.open('get','./json/students.json');
-        xhr.onload = ()=>{
+        title = 'MomoTalk';
+        header.insertAdjacentHTML('afterbegin', title);
+        if (document.body.clientWidth <= 720) {
+            console.log(true);
+
+            _msgv = document.createElement('div');
+            _msgv.className = 'msg-view';
+            _msgv.innerHTML = continer.querySelector('.msg-view').innerHTML;
+            continer.querySelector('.msg-view').remove();
+            document.body.append(_msgv);
+            _msgv.classList.add('mob');
+            continer.querySelector('.chr-list').style.gridColumn = 'span 2';
+            document.body.classList.add('mob');
+        }
+        xhr.open('get', './json/students.json');
+        xhr.onload = () => {
             _data = JSON.parse(xhr.response);
-            this.people.status=_data.status;
-            console.log(this.people.status)
+            this.people.rawDB = _data.students;
+            
             this.renderUserLists();
         }
         xhr.send();
     }
-    momoTalk.prototype.renderUserLists = function(){
-        console.log(0)
+    momoTalk.prototype.renderUserLists = function () {
+        console.log( this.people.rawDB);
+        _c= this.people.rawDB.students[0];
     }
     return momoTalk;
 }
 
-function pageInit(){
+function pageInit() {
 
 }
-function pageOnload(){
-    MomoTalk = new(momoTalk());
+function pageOnload() {
+    MomoTalk = new (momoTalk());
     MomoTalk.init();
     // pageInit();
 }
 
-window.onload = ()=>{
+window.onload = () => {
     setTimeout(() => {
         pageOnload();
     }, 1000);
