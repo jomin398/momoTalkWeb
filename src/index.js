475572071558bf41bbf1e27530d9ec160f2c523d
jomin398 <jomin398@gmail.com>
2021-11-18 17:29:29 +0900

    upload

--- src/index.js
+++ src/index.js
@@ -6,13 +6,14 @@ function momoTalk() {
     this.isMob = false;
     this.people = {
       DB: null,
-      unread: null
+      unread: null,
+      raw: null
     }
   }
-  momoTalk.prototype.reqDB = function(url, method, data) {
+  momoTalk.prototype.reqDB = function (url, method, data) {
     let xhr = new XMLHttpRequest();
     xhr.open(method ? method : "GET", url, true);
-    return new Promise(function(resolve, reject) {
+    return new Promise(function (resolve, reject) {
       var s, p, i;
       if (data && data.constructor == Object) {
         // serialize object
@@ -27,7 +28,7 @@ function momoTalk() {
             }
           } data = s;
       }
-      xhr.onreadystatechange = function() {
+      xhr.onreadystatechange = function () {
         if (xhr.readyState == 4) {
           resolve(xhr);
         }
@@ -35,7 +36,7 @@ function momoTalk() {
       xhr.send(data);
     });
   };
-  momoTalk.prototype.init = function() {
+  momoTalk.prototype.init = function () {
     document.querySelector('.init_display').remove();
     document.body.className = 'main';
     const continer = document.querySelector('.momoTalk.main');
@@ -57,81 +58,105 @@ function momoTalk() {
       document.body.classList.add('mob');
     }
     this.reqDB('./json/students.json').then(xhr => {
-      _data = JSON.parse(xhr.response);
-      this.people.DB = _data.students;
+      this.people.raw = JSON.parse(xhr.response);
+      this.people.DB = this.people.raw.students;
+      if (this.people.raw.extra_students) {
+        this.people.DB = this.people.DB.concat(this.people.raw.extra_students)
+      }
       this.reqDB('./json/lasttalk.json').then(xhr => {
         _data = JSON.parse(xhr.response);
         this.people.unread = _data;
         this.renderUserLists();
       });
     });
-  }
-  momoTalk.prototype.renderUserLists = function() {
-    console.log(this.people.DB.length);
-    _c = this.people.unread[0];
-    _ew = document.createElement('li');
-    _ew.className = 'chats__chat chat';
-    _e = document.createElement('a');
-    _fw = document.createElement('div');
-    _fw.className = 'chats_chatfriend friend friend--lg';
+  };
+
+  momoTalk.prototype.renderUserLists = function () {
+   
     _dirBase = './assets/images/';
-    _name = !isNaN(_c[0]) ? this.people.DB[_c[0]].n : _c[0];
-    _msg = _c[3];
-    _time = new Date(_c[1] != -1 ? _c[1] : new Date().getTime());
-    _time = _time.toLocaleTimeString('ko-KR', { hour: "numeric", minute: "numeric", hour12: true })
-    console.log([_name, _time, _msg]);
-    for (i = 0; i < 2; i++) {
-      _fwc = document.createElement('div');
-      _fwc.className = 'friend__column';
-      //0: left, 1:right; 
-      if (i == 0) {
-        _fci = document.createElement('img');
-        console.log(isNaN(_c[0]));
-        console.log(_dirBase + this.people.DB[_c[0]].c+'.png')
-        _fci.src =!isNaN(_c[0]) ?_dirBase + this.people.DB[_c[0]].c+'.png':'';
-        _fci.className='m-avatar friend__avatar';
-        _fc = document.createElement('div');
-        _fc.className = 'friend__content';
-
-        //name
-        _fcn = document.createElement('div');
-        _fcn.className = 'friend__name';
-        _fcn.innerText = _name;
-
-        //room message
-        _fct = document.createElement('div');
-        _fct.className = 'friend__bottom-text';
-        _fct.innerText = _msg ? _msg : '상태 메시지';
-
-        _fc.append(_fcn, _fct)
-        _fwc.append(_fci, _fc)
-        
-      } else if (i == 2) {
-        _sp = document.createElement('span');
-        _sp.className = 'chat__timestamp';
-        _sp.innerText = _time;
-        _crw = document.createElement('div');
-        _crw.className = 'chat__remain unread';
-        _cr = document.createElement('div');
-        _cr.className = 'chat__remain-count';
-        _crw.appendChild(_cr)
-        _fwc.append(_sp, _crw)
-      }
-      _fw.appendChild(_fwc)
+    _countTotal = 0;
+    if (this.people.unread) {
+      this.people.unread.forEach((e, i) => {
+        _countTotal += e[2] != -1 ? e[2] + 1 : 0;
+      });
+      let el = this.mainElm.querySelector('.sidebar-i span#tCount');
+      el.style.display = 'block';
+      el.innerText = _countTotal;
+      el = this.mainElm.querySelector('.chr-list .list_header #unreadCt');
+      el.innerText += ' (' + _countTotal + ')';
     }
-    _e.appendChild(_fw)
-    _ew.appendChild(_e)
-    document.querySelector('.chr-list_lists').appendChild(_ew)
+    console.log(this.people.DB.length);
+    for (let i in this.people.unread) {
+      _ew = document.createElement('li');
+      _ew.className = 'chats__chat chat';
+      _e = document.createElement('a');
+      _fw = document.createElement('div');
+      _fw.className = 'chats_chatfriend friend friend--lg';
+      _c = this.people.unread[i];
+      _name = !isNaN(_c[0]) ? this.people.DB[_c[0]].n : _c[0];
+      _msg = _c[3];
+      _time = new Date(_c[1] != -1 ? _c[1] : new Date().getTime());
+      _time = _time.toLocaleTimeString('ko-KR', { hour: "numeric", minute: "numeric", hour12: true })
+      _count = _c[2];
+
+      console.log([_name, _time, _msg, _count, _countTotal]);
+      for (j = 0; j < 2; j++) {
+        _fwc = document.createElement('div');
+        _fwc.className = 'friend__column';
+        //0: left, 1:right; 
+        if (j == 0) {
+          _fci = document.createElement('img');
+          console.log(_c[0]);
+          console.log(isNaN(_c[0]));
+          console.log(_dirBase + this.people.DB[_c[0]].c + '.png')
+          _fci.src = !isNaN(_c[0]) && _c[0]<=58? _dirBase + this.people.DB[_c[0]].c + '.png' : '';
+          _fci.className = 'm-avatar friend__avatar';
+          _fc = document.createElement('div');
+          _fc.className = 'friend__content';
+
+          //name
+          _fcn = document.createElement('div');
+          _fcn.className = 'friend__name';
+          _fcn.innerText = _name;
+
+          //room message
+          _fct = document.createElement('div');
+          _fct.className = 'friend__bottom-text';
+          _fct.innerText = _msg ? (_msg[0].m) : '상태 메시지';
+          _fc.append(_fcn, _fct);
+          _fwc.append(_fci, _fc);
+
+        } else if (j == 1) {
+          _sp = document.createElement('span');
+          _sp.className = 'chat__timestamp';
+          _sp.innerText = _time;
+          _crw = document.createElement('div');
+          _crw.className = 'chat__remain';
+          if (_c[2] != -1) {
+            _crw.classList.add('unread');
+            _cr = document.createElement('div');
+            _cr.className = 'chat__remain-count';
+            _cr.innerText = _count + 1;
+            _crw.appendChild(_cr);
+          };
+          _fwc.append(_sp, _crw);
+        }
+        _fw.appendChild(_fwc);
+      }
+      _e.appendChild(_fw);
+      _ew.appendChild(_e);
+      document.querySelector('.chr-list_lists').appendChild(_ew);
+    }
   }
   return momoTalk;
-}
+};
 
 function pageInit() {
 
 }
 
 function pageOnload() {
-  MomoTalk = new(momoTalk());
+  MomoTalk = new (momoTalk());
   MomoTalk.init();
   // pageInit();
 }
